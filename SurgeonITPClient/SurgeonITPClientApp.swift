7e8a5b1825f3e8c82655f78b9c915ef4cce8c0d2
Jorge Castillo <jorge.castillo.700@gmail.com>
2024-10-28 14:00:17 -0700

    Initial Commit

---
+++ SurgeonITPClient/SurgeonITPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurgeonITPClientApp.swift
+//  SurgeonITPClient
+//
+//  Created by Jorge Castillo on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SurgeonITPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurgeonITPClientTests/SurgeonITPClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurgeonITPClientTests.swift
+//  SurgeonITPClientTests
+//
+//  Created by Jorge Castillo on 10/28/24.
+//
+
+import Testing
+@testable import SurgeonITPClient
+
+struct SurgeonITPClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
